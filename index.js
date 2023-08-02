c2bb82590406f47779bfe96140e78688d688d3d0
yellhtetaung <yairhtet248.yha@gmail.com>
2023-08-02 22:01:54 +0630

    React and Write Files

---
+++ index.js
@@ -0,0 +1,41 @@
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf-8"
+    );
+
+    // remove
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
+    console.log(data);
+
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      data
+    );
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      "\n\nNice to meet you."
+    );
+
+    await fsPromises.rename(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      path.join(__dirname, "files", "promiseComplete.txt")
+    );
+
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "promiseComplete.txt"),
+      "utf-8"
+    );
+
+    console.log(newData);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fileOps();
