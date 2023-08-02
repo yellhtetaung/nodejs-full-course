fbfdf1310cde410efbb69d657b1d915dc61508ad
yellhtetaung <yairhtet248.yha@gmail.com>
2023-08-02 22:37:22 +0630

    event emitter

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
+const logEvents = require("./logEvents");
 
-console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+const EventEmitter = require("events");
 
-console.log(uuid());
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  // Emit event
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
