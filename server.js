3e568fa0f10ce1fe3b37553a43a563cca24f0370
yellhtetaung <yairhtet248.yha@gmail.com>
2023-08-06 20:32:42 +0630

    Middleware

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile("./views/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "views", "index.html"));
