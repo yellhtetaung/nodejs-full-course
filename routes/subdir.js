f37e13e5a39d3f1bb0bb5911d435d9b9bbce731e
yellhtetaung <yairhtet248.yha@gmail.com>
2023-08-07 10:39:00 +0630

    routing

---
+++ routes/subdir.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "index.html"));
+});
+
+router.get("/text(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "subdir", "index.html"));
+});
+
+module.exports = router;
